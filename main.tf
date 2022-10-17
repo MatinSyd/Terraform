f34344ecbd0205b2ad283e91622012d5026d0410
MatinSyd <93215557+MatinSyd@users.noreply.github.com>
2022-10-17 13:41:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
-provider "aws"
- region = "us-east-1"
- access_key = "AKIAV6AUDQKLWLNGVP76"
- secret_key = "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"
+
+provider "aws"{
+region = "us-east-1"
+access_key = "AKIAV6AUDQKLWLNGVP76"
+secret_key = "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"
+}
