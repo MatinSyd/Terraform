59ff2000eeb8a6ba1f8fbd2d6bd3004c26dead7c
MatinSyd <93215557+MatinSyd@users.noreply.github.com>
2023-04-27 16:09:15 +0530

    Update sub.tf

--- sub.tf
+++ sub.tf
@@ -1,4 +1,4 @@
-# Creating 1st web subnet
+# Creating public subnet 
 resource "aws_subnet" "tf-pub-sn-1" {
  vpc_id = "vpc-0e971a2c2041b0474"
  cidr_block = "192.168.100.0/24"
@@ -8,14 +8,14 @@ tags = {
  Name = "tf-pub-sn-1"
 }
 }
-# Creating 2nd web subnet
-resource "aws_subnet" "tf-pub-sn-2" {
+# Creating private subnet 
+resource "aws_subnet" "tf-pri-sn-2" {
  vpc_id = "vpc-0e971a2c2041b0474"
  cidr_block = "192.168.200.0/24"
- map_public_ip_on_launch = true
+ map_public_ip_on_launch = false 
  availability_zone = "us-west-2b"
 tags = {
- Name = "tf-pub-sn-2"
+ Name = "tf-pri-sn-2"
 }
 }
 
